37dc1a3cb0f1ec0e804279f78fd083431dca8843
Sumit Kumar <developer.sumit.professional@gmail.com>
2025-09-06 17:39:52 +0530

    Background Color Fixed

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import dnaimage from "../../public/dnaimage.png"
 import { FaArrowRightLong } from "react-icons/fa6";
 const Hero = () => {
   return (
-    <div className='w-full h-[200dvh] pt-24 overflow-hidden flex flex-col justify-start items-center'>
-      <div className='border-b-2 border-b-black/20 w-full h-[100dvh] rounded-b-full scale-110 flex justify-start flex-col items-center pt-20 relative'>
+    <div className='w-full h-[200dvh] pt-24 overflow-hidden flex flex-col justify-start items-center bg-white'>
+      <div className='border-b-2 border-b-black/20 w-full h-[100dvh] rounded-b-full scale-110 flex justify-start flex-col items-center pt-20 relative bg-white'>
 
         {/* 🔵 Blue gradients */}
         <div className='rounded-full w-64 h-64 bg-blue-500 blur-3xl opacity-60 absolute top-10  z-0'></div>
@@ -35,13 +35,13 @@ const Hero = () => {
             Technology
           </h1>
         </div>
-        <div className='flex justify-center items-center rounded-full  '>
-            <form className='flex justify-center items-center rounded-full border-2 border-black/20 py-1 px-1 focus:border-black'>
-                <input type="text" placeholder='Describe your problem' className='outline-none pl-3 pr-3'/>
+        <div className='flex justify-center items-center rounded-full  bg-white'>
+            <form className='flex justify-center items-center rounded-full border-2 border-black/20 py-1 px-1 focus:border-black bg-white'>
+                <input type="text" placeholder='Describe your problem' className='outline-none pl-3 pr-3 bg-transparent'/>
                 <div className='w-[1.5px] h-6 bg-black/20'></div>
                 <input type="text"
                     placeholder="Suffering since"
-                    className='px-3 outline-none'/>
+                    className='px-3 outline-none bg-transparent'/>
                 <button className='bg-[#1d1d1d] text-white rounded-full px-3 py-3 hover:opacity-75'>Send to AI</button>
             </form>
         </div>
@@ -59,21 +59,21 @@ const Hero = () => {
           <button className='absolute bottom-20 left-24 text-2xl bg-white/20 backdrop-blur-md rounded-full py-3 px-5 border-2 border-black/20 flex justify-center items-center gap-2 hover:bg-black hover:text-white transition-all duration-150'>Book Consultant</button>
         </div>  
         <div className='w-1/2 h-full pt-20 bg-transparent'>
-          <h3 className='text-3xl ml-14 font-michrome mt-20 mb-2'>Live Well, Be Well</h3> 
-          <p className='ml-14 pr-20 text-black/75'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero consequuntur accusamus illum sapiente repudiandae! Enim at accusantium, alias quam ad quo exercitationem quis officia minima ullam a aliquam totam quibusdam!</p>
+          <h3 className='text-3xl ml-14 font-michrome mt-20 mb-2 bg-transparent'>Live Well, Be Well</h3> 
+          <p className='ml-14 pr-20 text-black/75 bg-transparent'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero consequuntur accusamus illum sapiente repudiandae! Enim at accusantium, alias quam ad quo exercitationem quis officia minima ullam a aliquam totam quibusdam!</p>
 
-          <div className='ml-14 mt-10 flex justify-start items-center gap-20'>
-            <div className='flex justify-center items-start flex-col'>
-              <h3 className='text-5xl'>100+</h3>
-              <p className='ml-1'>Hospitals</p>
+          <div className='ml-14 mt-10 flex justify-start items-center gap-20 bg-transparent'>
+            <div className='flex justify-center items-start flex-col bg-transparent'>
+              <h3 className='text-5xl bg-transparent'>100+</h3>
+              <p className='ml-1 bg-transparent'>Hospitals</p>
             </div>
-            <div className='flex justify-center items-start flex-col'>
-              <h3 className='text-5xl'>1000+</h3>
-              <p className='ml-1'>Doctors</p>
+            <div className='flex justify-center items-start flex-col bg-transparent'>
+              <h3 className='text-5xl bg-transparent'>1000+</h3>
+              <p className='ml-1 bg-transparent'>Doctors</p>
             </div>
-            <div className='flex justify-center items-start flex-col'>
-              <h3 className='text-5xl'>10000+</h3>
-              <p className='ml-1'>Users</p>
+            <div className='flex justify-center items-start flex-col bg-transparent'>
+              <h3 className='text-5xl bg-transparent'>10000+</h3>
+              <p className='ml-1 bg-transparent'>Users</p>
             </div>
           </div>
         </div>
